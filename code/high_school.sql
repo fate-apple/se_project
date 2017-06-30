962b339e02aa744ca32ae92646fe5b41639e631e
Moon <3026775966@qq.com>
2017-06-30 14:51:15 +0800

    更新，键自增

--- code/high_school.sql
+++ code/high_school.sql
@@ -1,12 +1,12 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2017/6/28 11:45:56                           */
+/* Created on:     2017/6/30 14:43:08                           */
 /*==============================================================*/
 
 
 drop table if exists class;
 
-drop table if exists dean;
+drop table if exists course;
 
 drop table if exists period;
 
@@ -16,47 +16,51 @@ drop table if exists student;
 
 drop table if exists subject;
 
-drop table if exists teach;
+drop table if exists take;
 
 drop table if exists teacher;
-
-drop table if exists user;
 
 /*==============================================================*/
 /* Table: class                                                 */
 /*==============================================================*/
 create table class
 (
-   ID                   int not null,
-   building_number      int not null,
-   room_number          int not null,
-   tea_ID               int not null,
+   class_ID             int(11) not null AUTO_INCREMENT,
+   class_name           char(20),
+   building_number      char(20) not null,
+   room_number          char(20) not null,
+   tID                  int(11),
    password             char(20),
-   class_number         char(20),
-   amount               int,
-   primary key (ID)
+   amount               int(11),
+   primary key (class_ID)
 );
 
 /*==============================================================*/
-/* Table: dean                                                  */
+/* Table: course                                                */
 /*==============================================================*/
-create table dean
+create table course
 (
-   ID                   int not null,
-   password             char(20),
-   name                 char(20),
-   gender               bool,
-   birthdate            date,
-   primary key (ID)
+   course_ID            int(11) not null AUTO_INCREMENT,
+   building_number      char(20) not null,
+   room_number          char(20) not null,
+   period_ID            int(11) not null,
+   title                char(20) not null,
+   tID                  int(11),
+   primary key (course_ID)
 );
+
+
 
 /*==============================================================*/
 /* Table: period                                                */
 /*==============================================================*/
 create table period
 (
-   period_id            int not null,
-   primary key (period_id)
+   period_ID            int(11) not null AUTO_INCREMENT,
+   week_day             integer,
+   begin_time           time,
+   end_time             time,
+   primary key (period_ID)
 );
 
 /*==============================================================*/
@@ -64,25 +68,28 @@ create table period
 /*==============================================================*/
 create table room
 (
-   building_number      int not null,
-   room_number          int not null,
-   ID                   int,
-   capacity             int,
+   building_number      char(20) not null,
+   room_number          char(20) not null,
+   capacity             int(11),
    primary key (building_number, room_number)
 );
+
+
+
 
 /*==============================================================*/
 /* Table: student                                               */
 /*==============================================================*/
 create table student
 (
-   ID                   int not null,
-   cla_ID               int not null,
+   sID                  int(11) not null AUTO_INCREMENT,
+   stu_number           char(20),
+   class_ID             int(11) not null,
    password             char(20),
    name                 char(20),
    gender               bool,
    birthdate            date,
-   primary key (ID)
+   primary key (sID)
 );
 
 /*==============================================================*/
@@ -95,17 +102,13 @@ create table subject
 );
 
 /*==============================================================*/
-/* Table: teach                                                 */
+/* Table: take                                                  */
 /*==============================================================*/
-create table teach
+create table take
 (
-   tea_ID               int not null,
-   building_number      int not null,
-   room_number          int not null,
-   ID                   int not null,
-   period_id            int not null,
-   title                char(20) not null,
-   primary key (tea_ID, building_number, room_number, ID, period_id, title)
+   sID                  int(11) not null,
+   course_ID            int(11) not null,
+   primary key (sID, course_ID)
 );
 
 /*==============================================================*/
@@ -113,69 +116,46 @@ create table teach
 /*==============================================================*/
 create table teacher
 (
-   ID                   int not null,
-   building_number      int not null,
-   room_number          int not null,
-   cla_ID               int,
+   tID                  int(11) not null AUTO_INCREMENT,
+   employ_number        char(20) not null,
+   building_number      char(20) not null,
+   room_number          char(20) not null,
    password             char(20),
    name                 char(20),
    gender               bool,
-   birthdate            date,
-   primary key (ID)
+   authority            bool,
+   primary key (tID)
 );
 
-/*==============================================================*/
-/* Table: user                                                  */
-/*==============================================================*/
-create table user
-(
-   ID                   int not null,
-   password             char(20),
-   primary key (ID)
-);
 
-alter table class add constraint FK_Inheritance_2 foreign key (ID)
-      references user (ID) on delete restrict on update restrict;
 
 alter table class add constraint FK_Relationship_2 foreign key (building_number, room_number)
       references room (building_number, room_number) on delete restrict on update restrict;
 
-alter table class add constraint FK_Relationship_5 foreign key (tea_ID)
-      references teacher (ID) on delete restrict on update restrict;
+alter table class add constraint FK_Relationship_5 foreign key (tID)
+      references teacher (tID) on delete restrict on update restrict;
 
-alter table dean add constraint FK_Inheritance_4 foreign key (ID)
-      references user (ID) on delete restrict on update restrict;
+alter table course add constraint FK_teach foreign key (tID)
+      references teacher (tID) on delete restrict on update restrict;
 
-alter table room add constraint FK_Relationship_3 foreign key (ID)
-      references class (ID) on delete restrict on update restrict;
-
-alter table student add constraint FK_Inheritance_3 foreign key (ID)
-      references user (ID) on delete restrict on update restrict;
-
-alter table student add constraint FK_Relationship_4 foreign key (cla_ID)
-      references class (ID) on delete restrict on update restrict;
-
-alter table teach add constraint FK_teach foreign key (tea_ID)
-      references teacher (ID) on delete restrict on update restrict;
-
-alter table teach add constraint FK_teach2 foreign key (building_number, room_number)
+alter table course add constraint FK_teach2 foreign key (building_number, room_number)
       references room (building_number, room_number) on delete restrict on update restrict;
 
-alter table teach add constraint FK_teach3 foreign key (ID)
-      references student (ID) on delete restrict on update restrict;
+alter table course add constraint FK_teach4 foreign key (period_ID)
+      references period (period_ID) on delete restrict on update restrict;
 
-alter table teach add constraint FK_teach4 foreign key (period_id)
-      references period (period_id) on delete restrict on update restrict;
-
-alter table teach add constraint FK_teach5 foreign key (title)
+alter table course add constraint FK_teach5 foreign key (title)
       references subject (title) on delete restrict on update restrict;
 
-alter table teacher add constraint FK_Inheritance_1 foreign key (ID)
-      references user (ID) on delete restrict on update restrict;
+alter table student add constraint FK_Relationship_4 foreign key (class_ID)
+      references class (class_ID) on delete restrict on update restrict;
+
+alter table take add constraint FK_Reference_11 foreign key (sID)
+      references student (sID) on delete restrict on update restrict;
+
+alter table take add constraint FK_Reference_12 foreign key (course_ID)
+      references course (course_ID) on delete restrict on update restrict;
 
 alter table teacher add constraint FK_Relationship_1 foreign key (building_number, room_number)
       references room (building_number, room_number) on delete restrict on update restrict;
-
-alter table teacher add constraint FK_Relationship_6 foreign key (cla_ID)
-      references class (ID) on delete restrict on update restrict;
 
