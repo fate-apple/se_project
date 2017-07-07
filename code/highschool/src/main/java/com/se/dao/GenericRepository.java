187b52f17a7cdfe8aeea78c74518019bc3145a00
Moon <3026775966@qq.com>
2017-07-07 20:41:09 +0800

    整合后版本，能完成基本的页面跳转和登录操作。缺陷：首次登陆时会失败，第二次才能登陆成功

---
+++ code/highschool/src/main/java/com/se/dao/GenericRepository.java
@@ -0,0 +1,11 @@
+package com.se.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface GenericRepository<T, ID extends Serializable> extends MongoRepository<T, ID> {
+
+}
---
+++ code/highschool/src/main/java/com/se/domain/User.java
@@ -0,0 +1,132 @@
+package com.se.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.mongodb.core.index.TextIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Document(collection = "Users")
+public class User extends GenericModel implements UserDetails {
+
+	@NotNull
+	@Size(min = 2, max = 30)
+	@TextIndexed(weight = 2)
+	private String firstName;
+
+	@NotNull
+	@TextIndexed(weight = 3)
+	private String lastName;
+
+	@NotNull
+	@Size(min = 4, max = 30)
+	@TextIndexed
+	@Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\\\.[a-zA-Z0-9-]+)*$")
+	private String userName;
+
+	@NotNull
+	private String password;
+
+	private Role role;
+
+	public User(String firstName, String lastName, String userName, String password, Role role) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.role = role;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	@JsonProperty("username")
+	public String getUsername() {
+		return userName;
+	}
+
+	@JsonIgnore
+	public String getPassword() {
+		return password;
+	}
+
+	@JsonProperty
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@JsonIgnore
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+		Role userRoles = this.getRole();
+		if (userRoles != null) {
+			SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRoles.getRoleName());
+			authorities.add(authority);
+		}
+		return authorities;
+	}
+
+	public UsernamePasswordAuthenticationToken toAuthenticationToken() {
+		return new UsernamePasswordAuthenticationToken(userName, password, getAuthorities());
+	}
+
+	@JsonIgnore
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@JsonIgnore
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@JsonIgnore
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@JsonIgnore
+	public boolean isEnabled() {
+		return true;
+	}
+}
