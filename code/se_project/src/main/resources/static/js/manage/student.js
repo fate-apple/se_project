a27c7a79bbe513ecd1a643c255fa97bedaca5faa
SC-201506190939\Administrator <1245577954@qq.com>
2017-07-18 23:43:51 +0800

    模态框传数据

--- code/se_project/src/main/resources/static/js/manage/student.js
+++ code/se_project/src/main/resources/static/js/manage/student.js
@@ -4,18 +4,37 @@ jQuery(document).ready(function() {
     
     $("#add").click(function(e) {
     	$("#modalTitle").html("添加");
-    	//$("input[name='periodId']").val(0);
+    	$(".delete").css("display","none");
+    	$("#passwordDiv").css("display","block");
+    	$(".detailCourse").css("display","none");
+    	$("input[name='fullname']").val("");
+    	$("input[name='username']").val("");
+    	$("input[name='password']").val("");
+    	$("#gender").val("");
+    	$("#adminClass").val("");
+    	$("#virtualClass").val("");
+    	$("#enrollDate").val("");
+    	$(".save").attr("data-id","");
     	$("#modal").modal("show");
 	});
     
     $(".update").click(function(e){
     	$("#modalTitle").html("修改");
+    	$(".delete").css("display","inline");
+    	$("#passwordDiv").css("display","none");
+    	$(".detailCourse").css("display","inline");
     	var dataset = e.currentTarget.dataset;
-    	console.log(dataset.beginTime,dataset.endTime);
-    	//$("input[name='weekday']").val(dataset.weekday);
-    	//$("input[name='beginTime']").val(dataset.beginTime);
-    	//$("input[name='endTime']").val(dataset.endTime);
-    	//$("input[name='periodId']").val(dataset.id);
+    	var id = dataset.id;
+    	var gender = dataset.gender;
+    	var fullname = dataset.fullname;
+    	var roomid = dataset.roomid;
+    	var username = dataset.username;
+    	var enrolldate = dataset.enrolldate;
+    	$("#gender option[value="+gender+"]").attr("selected",true);
+    	$("input[name='fullname']").val(fullname);
+    	$("input[name='username']").val(username);
+    	$("#enrollDate").val(enrolldate);
+    	$(".save").attr("data-id", dataset.id);
     	$("#modal").modal("show");
     });
     
@@ -36,11 +55,11 @@ jQuery(document).ready(function() {
 					var id = dataset.id;
 					console.log(id);
 					jQuery.ajax({
-						url : '/manage/deleteStudent',
+						url : '/manage/class/deleteAdminclass',
 						processData : true,
 						dataType : "text",
 						data : {
-							studentId : id
+							id : id
 						},
 						success : function(data) {
 							console.log(id);
@@ -51,6 +70,40 @@ jQuery(document).ready(function() {
 					});
 				}
 			}
+		});
+	});
+	
+	$(".save").click(function(e){
+		var grade = $("#grade").val();
+		var fullname = $("input[name='fullname']").val();
+		var username = $("input[name='username']").val();
+		var password = $("input[name='password']").val();
+		var roomid = $("#room").val();
+		var dataset = e.currentTarget.dataset;
+		var id = dataset.id;
+		
+		jQuery.ajax({
+			url : "/manage/class/updateTeacher",
+				processData : true,
+				dataType : "text",
+				method : "POST",
+				data:{
+					id: id,
+					fullname : fullname,
+					username : username,
+					password : password,
+					roomid : roomid,
+					grade : grade
+				},
+				success : function(data) {
+					console.log(id);
+					bootbox.alert({
+						message : data,
+						callback : function() {
+							location.reload();
+						}
+					});
+				}
 		});
 	});
     
