0628c1731e5d5157ccbed616ea76dc2df18bd3f9
CLEVO-PC\clevo <1403656791@qq.com>
2017-07-20 16:13:02 +0800

    课程部分controller及Service

--- code/se_project/src/main/java/com/se/Controller/ApiResourceController.java
+++ code/se_project/src/main/java/com/se/Controller/ApiResourceController.java
@@ -4,6 +4,7 @@ import com.se.Repository.Jpa.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by clevo on 2017/7/20.
  */
-@RestController
-@RequestMapping(name="/api/resource",method = RequestMethod.GET)
+@Controller
+@RequestMapping(value="/api/resource",method=RequestMethod.GET)
 public class ApiResourceController {
     @Autowired
     PeriodRepository periodRepository;
@@ -32,7 +33,33 @@ public class ApiResourceController {
     UserRepository userRepository;
     @Autowired
     VirtualClassRepository virtualClassRepository;
+    @RequestMapping("/period")
     public ResponseEntity<?> period(){
         return ResponseEntity.ok(periodRepository.findAll());
+    }
+    @RequestMapping("/adminClass")
+    public ResponseEntity<?> adminClass(){
+        return ResponseEntity.ok(adminClassRepository.findAll());
+    }@RequestMapping("/course")
+    public ResponseEntity<?> course(){
+        return ResponseEntity.ok(courseRepository.findAll());
+    }@RequestMapping("/room")
+    public ResponseEntity<?> room(){
+        return ResponseEntity.ok(roomRepository.findAll());
+    }@RequestMapping("/student")
+    public ResponseEntity<?> student(){
+        return ResponseEntity.ok(studentRepository.findAll());
+    }@RequestMapping("/subject")
+    public ResponseEntity<?> subject(){
+        return ResponseEntity.ok(subjectRepository.findAll());
+    }@RequestMapping("/teacher")
+    public ResponseEntity<?> teacher(){
+        return ResponseEntity.ok(teacherRepository.findAll());
+    }@RequestMapping("/user")
+    public ResponseEntity<?> user(){
+        return ResponseEntity.ok(userRepository.findAll());
+    }@RequestMapping("/virtualClass")
+    public ResponseEntity<?> virtualClass(){
+        return ResponseEntity.ok(virtualClassRepository.findAll());
     }
 }
--- code/se_project/src/main/java/com/se/Domain/Business/Course.java
+++ code/se_project/src/main/java/com/se/Domain/Business/Course.java
@@ -31,6 +31,16 @@ public class Course {
 		this.weekday = weekday;
 	}
 
+	public Course(Room room, Teacher teacher, VirtualClass virtualClass, Subject subject, Period period, AdminClass adminClass, Integer weekday) {
+		this.room = room;
+		this.teacher = teacher;
+		this.virtualClass = virtualClass;
+		this.subject = subject;
+		this.period = period;
+		this.adminClass = adminClass;
+		this.weekday = weekday;
+	}
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "course_id")
