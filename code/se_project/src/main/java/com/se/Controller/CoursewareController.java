06c43e06c32b3f177cd7dbd4db9f9729b27a906c
SC-201506190939\Administrator <1245577954@qq.com>
2017-09-10 23:45:13 +0800

    课件上传

---
+++ code/se_project/src/main/java/com/se/Controller/CoursewareController.java
@@ -0,0 +1,10 @@
+package com.se.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class CoursewareController {
+
+}
--- code/se_project/src/main/resources/static/js/editDisplay/editBulletin.js
+++ code/se_project/src/main/resources/static/js/editDisplay/editBulletin.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
     
     $("#editIntroduction").click(function(e) {
     	var content = CKEDITOR.instances.informationEditor.getData();
-    	alert(content);
+    	
 
 	});
 
